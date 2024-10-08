be8854378479b38d1f5c33a14af9e071f190168a
RosalinaSpeedy <86687184+RosalinaSpeedy@users.noreply.github.com>
2024-10-08 13:38:17 +0100

    modify db script to allow for longer hashed passwords
    50 >100

--- create_db.sql
+++ create_db.sql
@@ -6,7 +6,7 @@ USE bettys_books;
 
 # Create the tables
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
-CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, username VARCHAR(50), firstName VARCHAR(50), lastName VARCHAR(50), email VARCHAR(50), hashedPassword VARCHAR(50), PRIMARY KEY(id));
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT, username VARCHAR(50), firstName VARCHAR(50), lastName VARCHAR(50), email VARCHAR(50), hashedPassword VARCHAR(100), PRIMARY KEY(id));
 
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
